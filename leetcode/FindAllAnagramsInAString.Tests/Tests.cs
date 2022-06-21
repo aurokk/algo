796c79075a06335cc7bcaa337d03f497f4604d2b
Dmitrii Kochnev <aurokkez@gmail.com>
2022-06-21 21:20:53 +0300

    Find All Anagrams In A String

---
+++ leetcode/FindAllAnagramsInAString.Tests/Tests.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace FindAllAnagramsInAString.Tests;
+
+public class Tests
+{
+    [TestCase("abab", "ab", new[] { 0, 1, 2 })]
+    [TestCase("cbaebabacd", "abc", new[] { 0, 6 })]
+    public void Test_000(string s, string p, IEnumerable<int> expected)
+    {
+        var actual = new Solution().FindAnagrams(s, p);
+        CollectionAssert.AreEqual(expected, actual);
+    }
+}
---
+++ leetcode/FindAllAnagramsInAString/Class1.cs
@@ -0,0 +1,46 @@
+﻿namespace FindAllAnagramsInAString;
+
+public class Solution
+{
+    public IList<int> FindAnagrams(string s, string p) // abab ab
+    {
+        // init
+        var counts = new Dictionary<char, int>();
+        for (var i = 'a'; i <= 'z'; i++)
+        {
+            counts[i] = 0;
+        }
+
+        // set counts
+        foreach (var ch in p)
+        {
+            counts[ch]++;
+        }
+
+        var result = new List<int>();
+        for (var i = 0; i < s.Length; i++)
+        {
+            var currCh = s[i];
+            var currLength = i + 1;
+
+            // subtract current
+            counts[currCh]--;
+
+            // add previous
+            if (currLength > p.Length)
+            {
+                var prevCh = s[i - p.Length];
+                counts[prevCh]++;
+            }
+
+            // check condition
+            var isAnagram = counts.Values.All(x => x == 0);
+            if (currLength >= p.Length && isAnagram)
+            {
+                result.Add(currLength - p.Length);
+            }
+        }
+
+        return result;
+    }
+}
