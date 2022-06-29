38a667ddf27aa0ce850b3de1d0383c5b1a645882
Dmitrii Kochnev <aurokkez@gmail.com>
2022-06-29 17:45:01 +0300

    Expression add operators

---
+++ leetcode/ExpressionAddOperators/Class1.cs
@@ -0,0 +1,113 @@
+﻿namespace ExpressionAddOperators;
+
+public class Solution
+{
+    public IList<string> AddOperators_000(string num, int target)
+    {
+        var results = new List<string>();
+
+        AddOperatorsInternal_000(results, num, 0, target, string.Empty, 0, 0);
+
+        return results;
+    }
+
+    private static void AddOperatorsInternal_000(
+        List<string> acc,
+        string num,
+        int numPosition,
+        int target,
+        string path,
+        long result,
+        long previous)
+    {
+        if (numPosition == num.Length)
+        {
+            if (result == target)
+            {
+                acc.Add(path);
+            }
+
+            return;
+        }
+
+        var ss = "";
+        long sn = 0;
+        for (var i = numPosition; i < num.Length; i++)
+        {
+            ss += num[i];
+            sn = sn * 10 + num[i] - '0';
+
+            if (ss.Length > 1 && ss[0] == '0')
+            {
+                break;
+            }
+
+            if (numPosition == 0)
+            {
+                AddOperatorsInternal_000(acc, num, i + 1, target, ss, sn, sn);
+            }
+            else
+            {
+                AddOperatorsInternal_000(acc, num, i + 1, target, path + "+" + ss, result + sn, sn);
+                AddOperatorsInternal_000(acc, num, i + 1, target, path + "-" + ss, result - sn, -sn);
+                AddOperatorsInternal_000(acc, num, i + 1, target, path + "*" + ss,
+                    (result - previous) + (previous * sn),
+                    previous * sn);
+            }
+        }
+    }
+
+    public IList<string> AddOperators_001(string num, int target)
+    {
+        var results = new List<string>();
+
+        var ss = "";
+        long sn = 0;
+        for (var i = 0; i < num.Length; i++)
+        {
+            ss += num[i];
+            sn = sn * 10 + (num[i] - '0');
+
+            if (ss.Length > 1 && ss[0] == '0')
+            {
+                break;
+            }
+
+            AddOperatorsInternal_001(results, num, i + 1, target, ss, sn, sn);
+        }
+
+        return results;
+    }
+
+    private static void AddOperatorsInternal_001(List<string> acc, string num, int position, int target, string path,
+        long result, long prev)
+    {
+        if (position == num.Length)
+        {
+            if (result == target)
+            {
+                acc.Add(path);
+            }
+
+            return;
+        }
+
+        var ss = "";
+        long sn = 0;
+        for (var i = position; i < num.Length; i++)
+        {
+            ss += num[i];
+            sn = sn * 10 + (num[i] - '0');
+
+            if (ss.Length > 1 && ss[0] == '0')
+            {
+                break;
+            }
+
+            AddOperatorsInternal_001(acc, num, i + 1, target, path + "+" + ss, result + sn, sn);
+            AddOperatorsInternal_001(acc, num, i + 1, target, path + "-" + ss, result - sn, -sn);
+            AddOperatorsInternal_001(acc, num, i + 1, target, path + "*" + ss, (result - prev) + (prev * sn),
+                prev * sn);
+        }
+    }
+}
