6aa6aac19cbdb025a59e64eadc34457bdb0d56a9
Dmitrii Kochnev <aurokkez@gmail.com>
2022-07-06 19:34:21 +0300

    Solved one problem

--- leetcode/PartitionArrayIntoTwoArraysToMinimizeSumDifference.Tests/UnitTest1.cs
+++ leetcode/PartitionArrayIntoTwoArraysToMinimizeSumDifference.Tests/UnitTest1.cs
@@ -5,9 +5,11 @@ public class Tests
     [TestCase(new[] { 3, 9, 7, 3 }, 2)]
     [TestCase(new[] { -36, 36 }, 72)]
     [TestCase(new[] { 2, -1, 0, 4, -2, -9 }, 0)]
+    [TestCase(new[] { 25, 49, 39, 42, 57, 35 }, 1)]
+    [TestCase(new[] { 7772197,4460211,-7641449,-8856364,546755,-3673029,527497,-9392076,3130315,-5309187,-4781283,5919119,3093450,1132720,6380128,-3954678,-1651499,-7944388,-3056827,1610628,7711173,6595873,302974,7656726,-2572679,0,2121026,-5743797,-8897395,-9699694 }, 1)]
     public void Test_000(int[] input, int expected)
     {
-        var actual = new Solution().MinimumDifference(input);
+        var actual = new Solution().MinimumDifference_000(input);
         Assert.That(actual, Is.EqualTo(expected));
     }
 }
--- leetcode/PartitionArrayIntoTwoArraysToMinimizeSumDifference/Program.cs
+++ leetcode/PartitionArrayIntoTwoArraysToMinimizeSumDifference/Program.cs
@@ -1,51 +1,81 @@
-﻿using System.Data.Common;
-
-namespace PartitionArrayIntoTwoArraysToMinimizeSumDifference;
+﻿namespace PartitionArrayIntoTwoArraysToMinimizeSumDifference;
 
 public class Solution
 {
-    public int MinimumDifference(int[] nums)
+    public int MinimumDifference_000(int[] nums)
     {
-        // // find target
-        // var sum = nums.Select(x => (long)x).Sum();
-        // var target = sum / 2;
-        //
-        // // init dp
-        // var dp = new long[nums.Length][];
-        // for (var i = 0; i < nums.Length; i++) dp[i] = new long[target];
-        //
-        // // calculate closest sum to target
-        // for (var curNumI = 0; curNumI < nums.Length; curNumI++)
-        // for (var curSum = 1; curSum < target; curSum++)
-        // {
-        //     var curNum = nums[curNumI];
-        //
-        //     var previousMax = curNumI - 1 >= 0
-        //         ? dp[curNumI - 1][curSum - 1]
-        //         : 0;
-        //
-        //     var previousMaxMinusCurNum = curNumI - 1 >= 0 && curSum - curNum >= 0
-        //         ? dp[curNumI - 1][curSum - curNum] + curNum
-        //         : 0;
-        //
-        //     var max = Math.Min(Math.Max(Math.Max(previousMax, previousMaxMinusCurNum), curNum), curSum);
-        //
-        //     dp[curNumI][curSum] = max;
-        // }
-        //
-        // // result
-        // var result = dp[^1][^1];
-        // return (int)(sum - result * 2);
-
         // find target
-        // var sum = nums.Select(x => (long)x).Sum();
-        // var target = sum / 2;
-        //
-        // for (var i = 0; i < UPPER; i++)
-        // {
-        //     
-        // }
+        var sum = nums.Select(x => x).Sum();
+        var targetSum = sum / 2;
+        var targetCount = nums.Length / 2;
 
-        throw new NotImplementedException();
+        // generate sums
+        var sums1 = new Dictionary<int, List<int>>();
+        for (var i = 0; i <= targetCount; i++)
+            sums1[i] = new List<int>();
+        sums1[0].Add(0);
+        GenerateSums_000(nums, 0, targetCount, 0, 0, sums1);
+        foreach (var (k, _) in sums1) sums1[k].Sort();
+
+        // generate sums
+        var sums2 = new Dictionary<int, List<int>>();
+        for (var i = 0; i <= targetCount; i++)
+            sums2[i] = new List<int>();
+        sums2[0].Add(0);
+        GenerateSums_000(nums, targetCount, nums.Length, 0, 0, sums2);
+        foreach (var (k, _) in sums1) sums2[k].Sort();
+
+        // meet in the middle
+        var diff = int.MaxValue;
+        foreach (var (n, _) in sums1)
+        {
+            var sums1L = sums1[n];
+            var k = targetCount - n;
+            var sums2L = sums2[k];
+
+            var l = 0;
+            var r = sums2L.Count - 1;
+            while (l < sums1L.Count && r >= 0)
+            {
+                var currSum = sums1L[l] + sums2L[r];
+                diff = Math.Min(diff, Math.Abs(sum - 2 * currSum));
+
+                if (currSum > targetSum)
+                    r--;
+                else if (currSum < targetSum)
+                    l++;
+                else
+                    break;
+            }
+
+            if (diff == 0)
+            {
+                break;
+            }
+        }
+
+        return diff;
+    }
+
+    private static void GenerateSums_000(
+        int[] nums,
+        int index,
+        int endIndex,
+        int items,
+        int sum,
+        Dictionary<int, List<int>> sums1)
+    {
+        if (items > 0)
+        {
+            sums1[items].Add(sum);
+        }
+
+        if (index == endIndex)
+        {
+            return;
+        }
+
+        GenerateSums_000(nums, index + 1, endIndex, items, sum, sums1);
+        GenerateSums_000(nums, index + 1, endIndex, items + 1, sum + nums[index], sums1);
     }
 }
