630f1da647f558d87a150fd76084ca3f206362b0
Dmitrii Kochnev <aurokkez@gmail.com>
2022-08-09 23:05:22 +0300

    Binary search tests

--- leetcode/AddBinary.Tests/UnitTest1.cs
+++ leetcode/AddBinary.Tests/UnitTest1.cs
@@ -9,4 +9,15 @@ public class Tests
         var actual = new Solution().AddBinary(a, b);
         Assert.That(actual, Is.EqualTo(expected));
     }
+
+    [Test]
+    public void Test_001()
+    {
+        var list = new List<int>();
+        Assert.That(~list.BinarySearch(0), Is.EqualTo(0));
+        list.Add(0);
+        Assert.That(~list.BinarySearch(1), Is.EqualTo(1));
+        list.Add(1);
+        Assert.That(list.BinarySearch(0), Is.EqualTo(0));
+    }
 }
